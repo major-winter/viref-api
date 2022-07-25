b127ca9564b1ba96a9f90e78def0c06ca04cd82e
Jason <jason@debian-BULLSEYE-live-builder-AMD64>
2022-07-25 11:27:33 +0700

    add cloud property to parse server

--- ParseServer.js
+++ ParseServer.js
@@ -7,7 +7,8 @@ var api = new ParseServer({
   appId: "Z4JgilyPeBPCyoHgN2D8uFfXsCs77S2bzczczkUW",
   masterKey: "1MYnluFjy5eus4QB1kVukfQ2WoKwkMd4DctdosVb",
   serverURL: "https://parseapi.back4app.com",
-  restAPIKey: "mZSMu9AMEajWWgbGyS5DBloTLcyc2jyJCDasTUoH"
+  restAPIKey: "mZSMu9AMEajWWgbGyS5DBloTLcyc2jyJCDasTUoH",
+  cloud: __dirname + '/cloud/main.js'
 
 });
 // Serve the Parse API on the /parse URL prefix
