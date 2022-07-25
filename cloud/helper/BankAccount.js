4cc88ffbf494ad0fc66199716863f7a6d15cf6e1
Jason <jason@debian-BULLSEYE-live-builder-AMD64>
2022-07-25 15:43:44 +0700

    update

--- cloud/helper/BankAccount.js
+++
@@ -1,8 +0,0 @@
-const helper = require('../helper');
-
-module.exports = {
-	async get(id) {
-		let query = new Parse.Query("BankAccount");
-		return query.get(id, { useMasterKey: true });
-	}
-}
--- cloud/helper/Product.js
+++
@@ -1,8 +0,0 @@
-const helper = require('../helper')
-
-module.exports = {
-    get(id) {
-        let query = new Parse.Query('Product')
-        return query.get(id, { useMasterKey: true })
-    },
-}
