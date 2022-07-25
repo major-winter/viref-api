3978b6a21d0a2bd77703aa6049c6f4abc51763dc
Jason <jason@debian-BULLSEYE-live-builder-AMD64>
2022-07-25 13:44:38 +0700

    remove user defined port

--- ParseServer.js
+++ ParseServer.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var ParseServer = require("parse-server").ParseServer;
 var app = express();
+console.log(__dirname)
 var api = new ParseServer({
   databaseURI:
     "mongodb://admin:iMS4F5qFHt3pjqO2Ec2b55Ts@MongoS3601A.back4app.com:27017/a93d2ae265d74fda82e7fe4d322bf1d7", // Connection string for your MongoDB database
@@ -12,6 +13,6 @@ var api = new ParseServer({
 });
 // Serve the Parse API on the /parse URL prefix
 app.use("/parse", api);
-app.listen(5000, function () {
-  console.log("parse-server-example running on port 5000");
+// app.listen(5000, function () {
+//   console.log("parse-server-example running on port 5000");
 });
