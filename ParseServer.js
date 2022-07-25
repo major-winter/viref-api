4c92a7a3af8d592237bb5ddc521387d9aa3cf9f9
Jason <jason@debian-BULLSEYE-live-builder-AMD64>
2022-07-25 14:15:22 +0700

    update

--- ParseServer.js
+++ ParseServer.js
@@ -1,7 +1,6 @@
 var express = require("express");
 var ParseServer = require("parse-server").ParseServer;
 var app = express();
-console.log(__dirname)
 var api = new ParseServer({
   databaseURI:
     "mongodb://admin:iMS4F5qFHt3pjqO2Ec2b55Ts@MongoS3601A.back4app.com:27017/a93d2ae265d74fda82e7fe4d322bf1d7", // Connection string for your MongoDB database
@@ -9,7 +8,7 @@ var api = new ParseServer({
   masterKey: "1MYnluFjy5eus4QB1kVukfQ2WoKwkMd4DctdosVb",
   serverURL: "https://viref.herokuapp.com/parse",
   restAPIKey: "mZSMu9AMEajWWgbGyS5DBloTLcyc2jyJCDasTUoH",
-  // cloud: __dirname + '/cloud/main.js',
+  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
 });
 // Serve the Parse API on the /parse URL prefix
 app.use("/parse", api);
